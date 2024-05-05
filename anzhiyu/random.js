ae98fc9fba347043ab4f595a652727c7fd565953
SkyeAurora <m15291144991@163.com>
2024-05-05 20:43:32 +0800

    Site updated: 2024-05-05 20:43:32

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/05/05/JobSearchLog/","2024/05/04/SevenPrinciples/","2024/04/29/hello-world/","2024/05/05/MinStack/"];function toRandomPost(){
+var posts=["2024/05/05/JobSearchLog/","2024/05/04/SevenPrinciples/","2024/05/05/MinStack/","2024/04/29/hello-world/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
